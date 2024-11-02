01d83d8b4b9a1bcbb3b79c3aa86e12d507009a63
devnamenor <nonelelacele@gmail.com>
2024-11-02 16:02:01 +0200

    started with validation

--- pkg/parser/parser_processor.go
+++ pkg/parser/parser_processor.go
@@ -1,1 +1,26 @@
 package parser
+
+import (
+	"os"
+
+	"github.com/goccy/go-yaml"
+)
+
+func parse(filePath string)(YamlParserConfig,error){
+    f, err :=os.Open(filePath)
+
+    if err!=nil {
+        return nil,err
+    }
+
+    defer f.Close()
+
+    decoder :=yaml.NewDecoder(f)
+
+    var parserConfig YamlParserConfig
+    err = decoder.Decode(&parserConfig)
+
+    if err!=nil {
+        eturn nil,err
+    }
+}
